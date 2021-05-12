f261683b715c0dcd927e3a2ecece851e562dcb83
Ye Shi <zoe.shiye@gmail.com>
2021-05-12 21:17:16 +1000

    added Electricians' login form

---
+++ src/components/Forms/ElectriciansLogin.js
@@ -0,0 +1,106 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import useForm from "./useForm";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start",
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  links: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start",
+    marginBottom: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const LoginForm = () => {
+  const { values, handleChange, handleSubmit } = useForm();
+
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Login to TasksTech
+        </Typography>
+        <p>*Required</p>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <div className={classes.links}>
+            <Link href="#" variant="body2">
+              Forgot password?
+            </Link>
+            <Link href="#" variant="body2">
+              {"Don't have an account? Sign up"}
+            </Link>
+          </div>
+          <div></div>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Next
+          </Button>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            [CUSTOMER - NEXT]
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+}
+
+export default ElectriciansLogin;
