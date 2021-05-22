fadb4b19cbf594bc7f379aa0dbe17ac3872ff510
Godsgift Akari <giftakari@yahoo.com>
2021-05-22 14:32:05 +1000

    Inventory Starter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <div>
     <App />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
