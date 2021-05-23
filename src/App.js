6db109674c6927bd39a78450b64342357c695a50
Godsgift Akari <giftakari@yahoo.com>
2021-05-23 10:08:17 +1000

    fixed routes

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import Header from "./components/Header/Header";
 function App() {
   return (
     <div className='App'>
-       <Header />
+      <Header />
       <Switch>
         <Route exact path='/' component={NewInventory} />
-        <Route exact='login' component={<TradieLogin />} />
+        <Route exact='/login' component={TradieLogin} />
       </Switch>
     </div>
   );
