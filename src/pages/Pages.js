266d0aa5c8c023a8049df762fbfac079a7b16fe6
Kimchi Rice <kimchi@u12.co>
2021-06-28 13:55:18 +1000

    [Add] Pages to consolidate all the pages

---
+++ src/pages/Pages.js
@@ -0,0 +1,4 @@
+export {default as Login } from "../pages/login/Login";
+export {default as SignUp } from "../pages/signUp/SignUp";
+export {default as ForgetPassword } from "../pages/forgetPassword/ForgetPassword";
+export {default as ResetPassword } from "../pages/resetPassword/ResetPassword";
