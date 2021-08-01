536bb4532d9fbebf2850f4cc43316eee4795acb9
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-08-01 10:02:02 +1000

    Delete all customer components, functions and path.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import TradieLogin from "./components/Forms/TradieLogin";
 import Header from "./components/Header/Header";
 import RightSideBar from "./components/rightSideBar/RightSideBar";
-import CustomerSignUp from "./components/Forms/CustomerSignUp";
-import CustomerLogin from "./components/Forms/CustomerLogIn";
 import TradieSignUp from "./components/Forms/TradieSignUp";
-import CustomerProfileForm from "./components/Forms/CustomerProfileForm";
+import TradieLogin from "./components/Forms/TradieLogin";
 import TradieProfileForm from "./components/Forms/TradieProfileForm";
 
 
@@ -18,11 +15,7 @@ function App() {
       <RightSideBar />
       <Switch>
         <Route exact path='/login' component={TradieLogin} />
-        <Route exact path='/customer/signup' component={CustomerSignUp} />
-        <Route exact path='/customer/login' component={CustomerLogin} />
         <Route exact path='/tradie/signup' component={TradieSignUp} />
-        <Route exact path='/tradie/login' component={TradieLogin} />
-        <Route exact path='/customer/profile' component={CustomerProfileForm} />
         <Route exact path='/tradie/profile' component={TradieProfileForm} />
 
 
