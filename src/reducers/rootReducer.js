729a316c22b04a94b4421e8bd33c95c722481751
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-08-16 19:33:12 +1000

    Add push function by history and router
    install module history to enable jump pages by routers.

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,12 +1,15 @@
+import { connectRouter } from 'connected-react-router';
 import { combineReducers } from 'redux';
 import jobsReducer from '../reducers/jobsReducer';
 import jobStatusReducer from '../reducers/jobStatusReducer';
-import traderReducer from './traderReducer';
+import traderReducer from '../reducers/traderReducer';
 
-const rootReducer = combineReducers({
+
+const rootReducer = history => combineReducers({
+    router:connectRouter(history),
+    trader:traderReducer,
     status: jobStatusReducer,
-    job: jobsReducer,
-    trader:traderReducer
+    job: jobsReducer
 });
 
 export default rootReducer;
